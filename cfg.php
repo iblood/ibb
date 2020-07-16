93876cc7c381c7801cb045678d1637fe36c5a15d
iblood <65953961+iblood@users.noreply.github.com>
2020-07-16 18:27:52 +0530

    Update cfg.php

--- cfg.php
+++ cfg.php
@@ -2,7 +2,7 @@
 $conn = mysqli_connect("sql100.epizy.com","epiz_26196161","lOlFo5brTl","epiz_26196161_");
 if(mysqli_connect_errno()){
 echo "OOPS! There might be a problem in connecting to our server. Please Retry again,<br>
-We apologise for the inconvenience caused".mysqli_connect_error();
+We apologise for the inconvenience caused.";
 die();
 }
 ?>
